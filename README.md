4578028edc555774af15a1a10c5d8556d6f6413d
unknown <wildreim@gmail.com>
2025-04-18 07:19:57 -0500

    Read power, add auto-hold

---
+++ README.md
@@ -0,0 +1,14 @@
+# argument_parser   
+Simple command-line argument parser that accepts a mix of both long-form and single-character short-form command options and returns results in an object   
+## Example   
+`command targeta targetb -fgh --option1 a b c --option2 d`   
+Returns:   
+``` {   
+  f: [],   
+  g: [],   
+  h: [],   
+  option1: [a, b, c],   
+  option2: [d],   
+  target: [targeta, targetb]   
+}   
+``` 
--- argument_parser.js
+++ argument_parser.js
@@ -2,43 +2,59 @@
     fudge
 */
 
-/*
-Parses command line arguments following these conventions:
-    #Accepts -- prefix for long-form options one-at-a-time
-    #Accepts - prefix for short-form options that can be groupded (-xyz)
-    #Unprefixed arguments are assigned to the previous option
-    #Initial unprefixed arguments are assigned to "target" option
-    #Results returned in a object with array results for each option
-*/
+//MD # argument_parser/p
+//MD Simple command-line argument parser that accepts a mix of both long-form
+//MD and single-character short-form command options and returns results in
+//MD an object/p
+//MD ## Example/p
+//MD `command targeta targetb -fgh --option1 a b c --option2 d`/p
+//MD Returns:/p
+//MD ```/p
+//MD {/p
+//MD   f: [],/p
+//MD   g: [],/p
+//MD   h: [],/p
+//MD   option1: [a, b, c],/p
+//MD   option2: [d],/p
+//MD   target: [targeta, targetb]/p
+//MD }/p
+//MD ```/p
+
 export default Object.freeze(function (arg_list) {
-    // Provide default option name
+
+// Provide default option name
     let initial = {
         target: []
     };
     let current = Object.keys(initial)[0];
 
     return Object.freeze(arg_list.reduce(
+
         function (results, nugget) {
 
-            // Long-form option
+// Long-form option
             if (nugget.startsWith("--")) {
                 current = nugget.slice(2);
                 results[current] = [];
 
-            // Short-form option(s)
+// Short-form option(s)
             } else if (nugget.startsWith("-")) {
                 nugget.slice(1).split("").forEach(function (flag) {
                     results[flag] = [];
                     current = flag;
                 });
 
-            // Unprefixed parameters
+// Unprefixed parameters
             } else {
                 results[current] = results[current].concat([nugget]);
 
             }
+
             return results;
-        },
+
+        };
+
         initial
+
     ));
 });
